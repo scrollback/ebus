4da4497ca1a8c0804e2e74f4977051be9518ab0c
harish kumar v <harry.softer@gmail.com>
2015-01-05 11:25:00 +0530

    Adding the debug option and tests.

--- test/emitter-test.js
+++ test/emitter-test.js
@@ -1,3 +1,4 @@
+/*global require, module, console*/
 /*
 
 This unit test describes the working of the Event Bus (Event Emitter).
@@ -88,4 +89,61 @@ describe("Testing Event Bus", function(){
 			} 
 		});
 	});
+	
+});
+
+describe("testing invalid params:", function() {
+	it("undefined priority", function(done) {
+		try{
+			core.on("text", function(a, n) {
+				n();
+			});
+		} catch(e){
+			assert.ok(true);
+		}
+		done();
+	});
+	
+	it("false priority", function(done) {
+		try{
+			core.on("text", function(a, n) {
+				n();
+			});
+		} catch(e){
+			assert.ok(true);
+		}
+		done();
+	});
+	it("object priority", function(done) {
+		try{
+			core.on("text", function(a, n) {
+				n();
+			});
+		} catch(e){
+			assert.ok(true);
+		}
+		done();
+	});
+	it("number priority", function(done) {
+		try{
+			core.on("text", function(a, n) {
+				n();
+			}, 100);
+			assert.ok(true);
+		} catch(e){
+			assert.ok(false);
+		}
+		done();
+	});
+	it("string priority", function(done) {
+		try{
+			core.on("text", function(a, n) {
+				n();
+			}, "validation");
+			assert.ok(true);
+		} catch(e){
+			assert.ok(false);
+		}
+		done();
+	});	
 });
